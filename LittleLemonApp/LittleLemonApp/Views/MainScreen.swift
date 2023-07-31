e0f39b243860056a515b3f58887341dcc0a67bc2
suletetes <sulea841@gmail.com>
2023-07-31 17:30:42 +0100

    final

--- LittleLemonApp/LittleLemonApp/Views/MainScreen.swift
+++ LittleLemonApp/LittleLemonApp/Views/MainScreen.swift
@@ -9,19 +9,25 @@ import SwiftUI
 
 struct MainScreen: View {
     @Environment(\.managedObjectContext) private var viewContext
-    
+    @EnvironmentObject private var viewModel: DishesModel // Add the DishesModel as an environment object
+
     var body: some View {
         NavigationStack {
             VStack {
                 Header()
                 Menu()
             }
+                    .onAppear {
+                        viewModel.reload(viewContext) // Call the reload function from the DishesModel on appearance
+                    }
         }
     }
 }
 
 struct MainScreen_Previews: PreviewProvider {
     static var previews: some View {
-        MainScreen().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+        MainScreen()
+                .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+                .environmentObject(DishesModel()) // Provide a DishesModel as an environment object for previews
     }
 }
