65453848225bf7559ef9ea9c317fd42558d14e53
suletetes <sulea841@gmail.com>
2023-07-31 17:27:26 +0100

    final

--- LittleLemonApp/LittleLemonApp/Views/FoodItem.swift
+++ LittleLemonApp/LittleLemonApp/Views/FoodItem.swift
@@ -5,44 +5,44 @@
 //  Created by suleabdul on 31.07.2023.
 //
 
+
 import SwiftUI
 
 struct FoodItem: View {
-    
-    let dish:Dish
-    
+
+    let dish: Dish
+
     var body: some View {
-        HStack {
-            VStack {
+        HStack(spacing: 15) {
+            VStack(alignment: .leading, spacing: 5) {
                 Text(dish.title ?? "")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.sectionCategories())
-                    .foregroundColor(.black)
-                Spacer(minLength: 10)
+                        .font(.sectionCategories())
+                        .foregroundColor(.black)
+
                 Text(dish.descriptionDish ?? "")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.paragraphText())
-                    .foregroundColor(.primaryColor1)
-                    .lineLimit(2)
-                Spacer(minLength: 5)
+                        .font(.paragraphText())
+                        .foregroundColor(.primaryColor1)
+                        .lineLimit(2)
+
                 Text("$" + (dish.price ?? ""))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.highlightText())
-                    .foregroundColor(.primaryColor1)
-                    .monospaced()
+                        .font(.highlightText())
+                        .foregroundColor(.primaryColor1)
+                        .monospaced()
             }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
             AsyncImage(url: URL(string: dish.image ?? "")) { image in
                 image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
             } placeholder: {
                 ProgressView()
             }
-            .frame(maxWidth: 90, maxHeight: 90)
-            .clipShape(Rectangle())
+                    .frame(width: 90, height: 90)
+                    .clipShape(Rectangle())
         }
-        .padding(.vertical)
-        .frame(maxHeight: 150)
+                .padding(.vertical)
+                .frame(maxHeight: 150)
     }
 }
 
@@ -50,5 +50,4 @@ struct FoodItem_Previews: PreviewProvider {
     static var previews: some View {
         FoodItem(dish: PersistenceController.oneDish())
     }
-    
 }
