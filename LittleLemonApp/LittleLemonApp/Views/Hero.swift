4e075ff947b0954ae2bae2aaf119c0ff6475c6b4
suletetes <sulea841@gmail.com>
2023-07-31 17:27:59 +0100

    final

--- LittleLemonApp/LittleLemonApp/Views/Hero.swift
+++ LittleLemonApp/LittleLemonApp/Views/Hero.swift
@@ -4,46 +4,40 @@
 //
 //  Created by suleabdul on 31.07.2023.
 //
+
 import SwiftUI
 
 struct Hero: View {
-    
+
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    Text("Little Lemon")
-                        .foregroundColor(Color.primaryColor2)
+        ZStack(alignment: .bottomLeading) {
+            Image("hero-image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .clipped()
+
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Little Lemon")
+                        .foregroundColor(.primaryColor2)
                         .font(.displayFont())
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Chicago")
+                Text("Chicago")
                         .foregroundColor(.white)
                         .font(.subTitleFont())
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Spacer(minLength: 5)
-                    Text("""
-                     We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
-                     """)
-                    .foregroundColor(.white)
-                    .font(.leadText())
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                Image("hero-image")
-                    .resizable()
-                    .aspectRatio( contentMode: .fill)
-                    .frame(maxWidth: 120, maxHeight: 140)
-                    .clipShape(Rectangle())
-                    .cornerRadius(16)
+                Text("We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+                        .foregroundColor(.white)
+                        .font(.leadText())
             }
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.primaryColor1)
         }
+                .frame(maxHeight: 240)
     }
 }
 
 struct Hero_Previews: PreviewProvider {
     static var previews: some View {
         Hero()
-            .padding()
-            .background(Color.primaryColor1)
-            .frame(maxWidth: .infinity, maxHeight: 240)
     }
 }
