d9fb5e0ecccbaaf6f1ade82613067691c13d3625
suletetes <sulea841@gmail.com>
2023-07-31 17:29:44 +0100

    final

--- LittleLemonApp/LittleLemonApp/Views/Home.swift
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -5,18 +5,27 @@
 //  Created by suleabdul on 31.07.2023.
 //
 
+
 import SwiftUI
 
 struct Home: View {
-    
+    @StateObject private var viewModel = DishesModel()
+
     var body: some View {
-        MainScreen()
-            .navigationBarBackButtonHidden()
+        NavigationView {
+            MainScreen()
+                    .navigationBarBackButtonHidden()
+                    .environmentObject(viewModel)
+        }
+                .onAppear {
+                    viewModel.reload(PersistenceController.shared.container.viewContext)
+                }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+        Home()
+                .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
 }
