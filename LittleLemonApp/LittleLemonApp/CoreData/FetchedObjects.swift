b5a06b8d218f92b77fa8267ed87f009bde3dd560
suletetes <sulea841@gmail.com>
2023-07-31 17:21:57 +0100

    final

--- LittleLemonApp/LittleLemonApp/CoreData/FetchedObjects.swift
+++ LittleLemonApp/LittleLemonApp/CoreData/FetchedObjects.swift
@@ -7,34 +7,31 @@
 //
 
 
+
 import CoreData
 import Foundation
 import SwiftUI
 
-struct FetchedObjects<T, Content>: View where T : NSManagedObject, Content : View {
-    
+struct FetchedObjects<T, Content>: View where T: NSManagedObject, Content: View {
+
   let content: ([T]) -> Content
 
-  var request: FetchRequest<T>
-  var results: FetchedResults<T>{ request.wrappedValue }
-    
+  @FetchRequest var results: FetchedResults<T>
+
   init(
-    predicate: NSPredicate = NSPredicate(value: true),
-    sortDescriptors: [NSSortDescriptor] = [],
-    @ViewBuilder content: @escaping ([T]) -> Content
+          predicate: NSPredicate = NSPredicate(value: true),
+          sortDescriptors: [NSSortDescriptor] = [],
+          @ViewBuilder content: @escaping ([T]) -> Content
   ) {
     self.content = content
-    self.request = FetchRequest(
-      entity: T.entity(),
-      sortDescriptors: sortDescriptors,
-      predicate: predicate
+    self._results = FetchRequest(
+            entity: T.entity(),
+            sortDescriptors: sortDescriptors,
+            predicate: predicate
     )
   }
-  
-  
+
   var body: some View {
-    self.content(results.map { $0 })
+    self.content(Array(results))
   }
-  
-  
 }
--- LittleLemonApp/LittleLemonApp/CoreData/Persistence.swift
+++ LittleLemonApp/LittleLemonApp/CoreData/Persistence.swift
@@ -4,6 +4,7 @@
 //
 //  Created by suleabdul on 31.07.2023.
 //
+
 import CoreData
 import Foundation
 
@@ -15,21 +16,29 @@ struct PersistenceController {
     init() {
         container = NSPersistentContainer(name: "ExampleDatabase")
         container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        container.loadPersistentStores(completionHandler: {_,_ in })
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
-    
+
     func clear() {
         // Delete all dishes from the store
         let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Dish")
         let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-        let _ = try? container.persistentStoreCoordinator.execute(deleteRequest, with: container.viewContext)
+        do {
+            try container.viewContext.execute(deleteRequest)
+        } catch {
+            print("Error deleting dishes: \(error)")
+        }
     }
-    
+
     static func oneDish() -> Dish {
         let dish = Dish(context: shared.container.viewContext)
         dish.title = "Greek Salad"
-        dish.descriptionDish = "The famous greek salad of crispy lettuce, peppers, olives, our Chicago."
+        dish.descriptionDish = "The famous Greek salad of crispy lettuce, peppers, olives, and feta cheese."
         dish.price = "10"
         dish.category = "starters"
         dish.image = "https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/greekSalad.jpg?raw=true"
