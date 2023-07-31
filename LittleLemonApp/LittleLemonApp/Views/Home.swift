9bbafacb9a79ef8cc11503194b2d9f9f79063756
suletetes <sulea841@gmail.com>
2023-07-31 17:13:45 +0100

    final

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonAppFinal
+//
+//  Created by suleabdul on 31.07.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
